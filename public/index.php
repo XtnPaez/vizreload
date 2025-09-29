cabcf7a4d97dca0d7003d16256302d6e64b92044
XtnPaez <paez.cristian@gmail.com>
2025-09-29 14:22:24 -0300

    logos incluidos y capas seteadas

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8" />
-        <title>EFPI : Home</title>    
+        <title>EFPI : Portada</title>    
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
         <!-- Leaflet CSS -->
